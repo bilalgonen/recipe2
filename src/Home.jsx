1664c4cc0de737b46c35fc0f2c6c0302b4b36d0e
Bilal Gonen <bilalgonen@gmail.com>
2024-08-21 12:36:10 -0400

    Back to Recipes goes to previous page=x correctly

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,26 +6,49 @@ import { useSearchParams } from 'react-router-dom'
 import Pagination from './Pagination'
 
 export default function Home() {
-  const [url, setUrl] = useState('https://dummyjson.com/recipes?page=1')
+  const [url, setUrl] = useState(null)
   const [searchParams, setSearchParams] = useSearchParams()
   const [currentPage, setCurrentPage] = useState(searchParams.get('page') || 1)
-  const [postsPerPage, setPostsPerPage] = useState(6)
+  // const [currentPage, setCurrentPage] = useState(1)
   const { items, loading, Error } = useFetch(url)
+  const postsPerPage = 6
 
   useEffect(() => {
+    // if (!searchParams.get('page')) {
+    //   console.log('first if')
+    //   setCurrentPage(1)
+    // } else {
+    //   setCurrentPage(searchParams.get('page'))
+    // }
+
+    // console.log('currentPage:', currentPage)
     setSearchParams((searchParams) => {
       searchParams.set('page', currentPage)
-
       return searchParams
     })
+    console.log('useEff[currPage] searchParams page:', searchParams.get('page'))
     // console.log('url1:', url)
+    // setUrl('https://dummyjson.com/recipes')
     setUrl(
       `https://dummyjson.com/recipes?limit=${postsPerPage}&skip=${
         (currentPage - 1) * postsPerPage
       }`
     )
-    // console.log('url2:', url)
+    // console.log('url:', url)
   }, [currentPage])
+
+  // useEffect(() => {
+  //   console.log('useEffect[] searchParams page:', searchParams.get('page'))
+  //   setCurrentPage(searchParams.get('page') || 1)
+  //   console.log('currentPage:', currentPage)
+  //   console.log('url1:', url)
+  //   setUrl(
+  //     `https://dummyjson.com/recipes?limit=${postsPerPage}&skip=${
+  //       (currentPage - 1) * postsPerPage
+  //     }`
+  //   )
+  //   console.log('url2:', url)
+  // }, [])
 
   return (
     <div>
